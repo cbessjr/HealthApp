f479d49e47faf745628cb3bbe93cb591a79aa597
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2019-06-13 21:50:24 -0700

    fixed errors causing api server crash

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const db = require("./db/models");
+const db = require("./models");
 const routes = require("./routes");
 
 // ===============================Middleware=======================
