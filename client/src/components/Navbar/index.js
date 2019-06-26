69f22d884d5ae842554c6bb6ad6fe3808371c98c
james barker <jamesbarker585@gmail.com>
2019-06-25 17:05:32 -0700

    navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,27 +1,58 @@
-import React, { Component } from 'react'
-import NavbarContent from './NavbarContent'
-import NavbarIcon from './NavbarIcon'
-
-export default class Navbar extends Component {
-	state = {
-		isopen: true
+import React from 'react'
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem } from 'reactstrap';
+  
+  export default class Example extends React.Component {
+	constructor(props) {
+	  super(props);
+  
+	  this.toggle = this.toggle.bind(this);
+	  this.state = {
+		isOpen: false
+	  };
 	}
-
-	toggleNavbar = () => {
-		this.setState(prevState => ({
-			isOpen: !prevState.isOpen
-		}))
+	toggle() {
+	  this.setState({
+		isOpen: !this.state.isOpen
+	  });
 	}
-
 	render() {
-		return <dev className="navbar-container">
-			<NavbarContent isOpen={this.state.isopen} />
-			<dev className="navbar-icon">
-				<NavbarIcon
-					isOpen={this.state.isopen}
-					handleClick={this.toggleNavbar}
-				/>
-			</dev>
-		</dev>
+	  return (
+		<div>
+		  <Navbar color="light" light expand="md">
+			<NavbarBrand href="/">GetHealthy</NavbarBrand>
+			<NavbarToggler onClick={this.toggle} />
+			<Collapse isOpen={this.state.isOpen} navbar>
+			  <Nav className="ml-auto" navbar>
+				<UncontrolledDropdown nav inNavbar>
+				  <DropdownToggle nav caret>
+					Medications
+				  </DropdownToggle>
+				  <DropdownMenu right>
+					<DropdownItem>
+					  Schedule
+					</DropdownItem>
+					<DropdownItem>
+					  Medication Lookup
+					</DropdownItem>
+					<DropdownItem divider />
+					<DropdownItem>
+					  Find A Pharmacy
+					</DropdownItem>
+				  </DropdownMenu>
+				</UncontrolledDropdown>
+			  </Nav>
+			</Collapse>
+		  </Navbar>
+		</div>
+	  );
 	}
-}
+  }
