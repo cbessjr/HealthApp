7b79e4739c6186c5914765c52ba68c274e54161b
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-01 18:23:04 -0700

    fixed issue with login button and added mock login and logout logic

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,156 +1,150 @@
-import React from 'react'
+import React from "react";
 import {
-	Collapse,
-	Navbar,
-	NavbarToggler,
-	NavbarBrand,
-	Nav,
-	UncontrolledDropdown,
-	DropdownToggle,
-	DropdownMenu,
-	DropdownItem, 
-	ButtonDropdown, 
-	Button,
-	Form, 
-	FormGroup,
-	Label,
-	Col,
-	Input,
-} from 'reactstrap'
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownItem,
+  UncontrolledButtonDropdown,
+  DropdownMenu,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Col,
+  Input
+} from "reactstrap";
 
-export default class Example extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.toggle = this.toggle.bind(this);
-		this.state = {
-			isOpen: false
-		};
+export default class Navigation extends React.Component {
+  state = {
+    isOpen: false
+  };
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+      isLoggedIn: false
+    });
 	}
-	toggle() {
-		this.setState({
-			isOpen: !this.state.isOpen
-		});
+	
+  handleLogIn() {
+    this.setState({
+      isLoggedIn: true
+    });
 	}
-	render() {
-		return (
-			<div>
-				<Navbar color="light" light expand="md">
-					<NavbarBrand href="/">GetHealthy</NavbarBrand>
-					<NavbarToggler onClick={this.toggle} />
-					<Collapse isOpen={this.state.isOpen} navbar>
-						<Nav className="ml-auto" navbar>
-							<UncontrolledDropdown nav inNavbar>
-								<DropdownToggle nav caret>
-									Medications
-				  </DropdownToggle>
-								<DropdownMenu right>
-									<DropdownItem>
-										Schedule
-					</DropdownItem>
-									<DropdownItem>
-										Medication Lookup
-					</DropdownItem>
-									<DropdownItem>
-										Find A Pharmacy
-					</DropdownItem>
-									<DropdownItem>
-										Report
-					</DropdownItem>
-								</DropdownMenu>
-							</UncontrolledDropdown>
-						</Nav>
-					</Collapse>
-					<NavbarToggler onClick={this.toggle} />
-					<Collapse isOpen={this.state.isOpen} navbar>
-						<Nav className="ml-auto" navbar>
-							<UncontrolledDropdown nav inNavbar>
-								<DropdownToggle nav caret>
-									Exercise
-				  </DropdownToggle>
-								<DropdownMenu right>
-									<DropdownItem>
-										New Exercise
-					</DropdownItem>
-									<DropdownItem>
-										Exercise Lookup
-					</DropdownItem>
-									<DropdownItem>
-										Report
-					</DropdownItem>
-								</DropdownMenu>
-							</UncontrolledDropdown>
-						</Nav>
-					</Collapse>
-					<NavbarToggler onClick={this.toggle} />
-					<Collapse isOpen={this.state.isOpen} navbar>
-						<Nav className="ml-auto" navbar>
-							<UncontrolledDropdown nav inNavbar>
-								<DropdownToggle nav caret>
-									Meals
-				  </DropdownToggle>
-								<DropdownMenu right>
-									<DropdownItem>
-										Add A Meal
-					</DropdownItem>
-									<DropdownItem>
-										Food Lookup
-					</DropdownItem>
-									<DropdownItem>
-										Water Consuption
-					</DropdownItem>
-									<DropdownItem>
-										Report
-					</DropdownItem>
-								</DropdownMenu>
-							</UncontrolledDropdown>
-						</Nav>
-					</Collapse>
-					<NavbarToggler onClick={this.toggle} />
-					<Collapse isOpen={this.state.isOpen} navbar>
-						<Nav className="ml-auto" navbar>
-							<UncontrolledDropdown nav inNavbar>
-								<DropdownToggle nav caret>
-									Reports
-				  </DropdownToggle>
-								<DropdownMenu right>
-									<DropdownItem>
-										Medications
-					</DropdownItem>
-									<DropdownItem>
-										Exercise
-					</DropdownItem>
-									<DropdownItem>
-										Meals
-					</DropdownItem>
-								</DropdownMenu>
-							</UncontrolledDropdown>
-						</Nav>
-					</Collapse>
-					<ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-						<DropdownToggle caret>
-							Login
-        </DropdownToggle>
-						<DropdownMenu>
-							<Form>
-								<FormGroup row>
-									<Label for="exampleEmail" sm={2}>Email</Label>
-									<Col sm={10}>
-										<Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-									</Col>
-								</FormGroup>
-								<FormGroup row>
-									<Label for="examplePassword" sm={2}>Password</Label>
-									<Col sm={10}>
-										<Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-									</Col>
-								</FormGroup>
-							</Form>
-							<Button color="primary">Submit</Button>{' '}
-						</DropdownMenu>
-					</ButtonDropdown>
-				</Navbar>
-			</div>
-		);
+	
+  handleLogOut() {
+    this.setState({
+      isLoggedIn: false
+    });
 	}
-} 
+	
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">GetHealthy</NavbarBrand>
+          {this.state.isLoggedIn ? (
+            <React.Fragment>
+              <NavbarToggler onClick={this.toggle.bind(this)} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="mr-auto" navbar>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Medications
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem>Schedule</DropdownItem>
+                      <DropdownItem>Medication Lookup</DropdownItem>
+                      <DropdownItem>Find A Pharmacy</DropdownItem>
+                      <DropdownItem>Report</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Exercise
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem>New Exercise</DropdownItem>
+                      <DropdownItem>Exercise Lookup</DropdownItem>
+                      <DropdownItem>Report</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Meals
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem>Add A Meal</DropdownItem>
+                      <DropdownItem>Food Lookup</DropdownItem>
+                      <DropdownItem>Water Consuption</DropdownItem>
+                      <DropdownItem>Report</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Reports
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem>Medications</DropdownItem>
+                      <DropdownItem>Exercise</DropdownItem>
+                      <DropdownItem>Meals</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </Nav>
+              </Collapse>
+              <Button
+                color="danger"
+                onClick={this.handleLogOut.bind(this)}
+              >
+                Log Out
+              </Button>
+            </React.Fragment>
+          ) : (
+            <UncontrolledButtonDropdown>
+              <DropdownToggle caret>Login</DropdownToggle>
+              <DropdownMenu>
+                <Form>
+                  <FormGroup row>
+                    <Label for="email" sm={2}>
+                      Email
+                    </Label>
+                    <Col sm={10}>
+                      <Input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="email address"
+                      />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Label for="password" sm={2}>
+                      Password
+                    </Label>
+                    <Col sm={10}>
+                      <Input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="password"
+                      />
+                    </Col>
+                  </FormGroup>
+                </Form>
+                <Button
+                  onClick={this.handleLogIn.bind(this)}
+                  color="primary"
+                >
+                  Submit
+                </Button>{" "}
+              </DropdownMenu>
+            </UncontrolledButtonDropdown>
+          )}
+        </Navbar>
+      </div>
+    );
+  }
+}
