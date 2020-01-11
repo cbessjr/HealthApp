a39cd7be0c0dfc3db7b26ba41215a007c71d1697
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-10 22:35:01 -0700

    updated logic to get images from Unsplash

--- client/src/components/Unsplash/index.js
+++ client/src/components/Unsplash/index.js
@@ -1,32 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
-import Image from '../Image';
+// import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
+// import Image from '../Image';
 import clientId from '../../util';
+import './style.css';
 
 const Pictures = () => {
+
   const [imageLoaded, setImageLoaded] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get(`https://api.unsplash.com/search/photos?query=health&client_id=${clientId}`)
+    axios.get(`https://api.unsplash.com/search/photos?query=health&client_id=${clientId}&orientation=landscape&fit=crop&h=140`)
       .then((data) => {
-        console.log(data);
         setImageLoaded(data.data.results);
         setLoading(false);
       });
   }, []);
 
   return(
-    <div>
-
+    <div class='slideshow mt-5'>
       {loading ? null :
         imageLoaded.map(image => (
-          // console.log(image)
-          <img height='113' src={image.urls.thumb} alt={image.alt_description} key={image.id} />
+          <span><img src={image.urls.thumb} alt={image.alt_description} key={image.id} /></span>
         ))
       }
-
     </div>
   );
 };
