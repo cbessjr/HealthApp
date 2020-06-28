09954c267f6cd97663966db6a8ae06084fe1feab
Matt Kilcup <nevermindthelabel@gmail.com>
2020-06-27 17:18:28 -0700

    cleaned up code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from '../components/Navbar';
-// import Carousel from '../components/Carousel/carousel';
 import logo from '../img/HealthAppLogoAnimated.gif';
 import Footer from '../components/Footer';
 import Unsplash from '../components/Unsplash';
-// import ModalAbout from '../components/ModalAbout';
 
 class Home extends Component {
 
@@ -12,16 +10,14 @@ class Home extends Component {
     return (
       <>
         <Navbar />
-      
-      <div className="App">
-        <div>
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="App">
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <h2 className="text-center">Welcome To GetHealthy</h2>
+          <Unsplash />
+          <Footer />
         </div>
-        <h2 className="text-center">Welcome To GetHealthy</h2>
-        {/* <Carousel /> */}
-        <Unsplash />
-        <Footer />
-      </div>
       </>
     );
   }
